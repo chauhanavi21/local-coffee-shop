1f00eebe2caf30129da89fd521b78f86aa7600f1
avi212003 <chauhanavi212003@gmail.com>
2026-06-06 18:18:58 -0400

    feat: render database connection strip in page body

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { DbConnectionBar } from "./DbConnectionBar";
 
 export function Layout() {
   return (
@@ -8,6 +9,7 @@ export function Layout() {
       <Navbar />
       <main className="flex-1">
         <Outlet />
+        <DbConnectionBar variant="body" />
       </main>
       <Footer />
     </div>
